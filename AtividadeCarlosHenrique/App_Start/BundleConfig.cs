dbde9ddbf1a0309494d61dff3502d93140239c3d
Carlos Henrique Almeida Oliveira <carlos.h.almeida@animaeducacao.com.br>
2023-05-12 11:33:21 -0300

    Algumas alterações

--- AtividadeCarlosHenrique/App_Start/BundleConfig.cs
+++ AtividadeCarlosHenrique/App_Start/BundleConfig.cs
@@ -16,15 +16,12 @@ namespace AtividadeCarlosHenrique
 
             // Use a versão em desenvolvimento do Modernizr para desenvolver e aprender com ela. Após isso, quando você estiver
             // pronto para a produção, utilize a ferramenta de build em https://modernizr.com para escolher somente os testes que precisa.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css"));
             bundles.Add(new StyleBundle("~/Content/css").Include(
           "~/Content/styles.css"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
