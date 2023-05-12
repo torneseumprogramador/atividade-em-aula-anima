9810f8320e32a973a46b7d3beb6c02bc9489ce45
Luanaap <57630311+Luanaap@users.noreply.github.com>
2023-05-12 11:43:37 -0300

    criacao da model

---
+++ Luana_WebApplication/WebApplication/WebApplication/Models/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Pessoa
+    {
+        private static List<Pessoa> pessoas = new List<Pessoa>();
+        public string Nome;
+        public string CPF;
+        public string Telefone;
+
+        public void Salvar()
+        {
+            pessoas.Add(this);
+        }
+
+        public static List<Pessoa> Todos()
+        {
+            return pessoas;
+        }
+    }
+}
